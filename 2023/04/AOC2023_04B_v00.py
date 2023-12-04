6a5404b80ce66b9d154053aa38da0382323b8423
alexbabington <33516326+alexbabington@users.noreply.github.com>
2023-12-04 16:58:04 +0000

    Add files via upload

---
+++ 2023/04/AOC2023_04B_v00.py
@@ -0,0 +1,77 @@
+#Advent of code 2023
+#Day 4 Part 1
+#Scratchcards
+
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+import pdb #pdb.set_trace()
+import time
+import copy
+
+start_time = time.time()
+
+###Constants
+
+###Functions
+def match_numbers(guesses,answers):
+    #Takes a list of guesses and returns and which appear in the answers list
+    matches = []
+    for i_b,guess in enumerate(guesses):
+        if guess in answers:
+            matches.append(guess)
+    
+    return matches
+
+###Import and sort File
+input = []
+
+with open("input_04.txt") as input_dataf:
+    for i,line in enumerate(input_dataf):
+        #Import operations
+        
+        line = line.strip("\n")#Remove EOL symbol
+        line = line.split(": ")#Split off card number
+        line = line[1]
+        line = line.split(" | ")
+        for i_a,side in enumerate(line):
+            line[i_a] = line[i_a].split()
+            line[i_a] = [eval(i) for i in line[i_a]]
+        
+        
+        input.append(line)
+
+cards = np.array(input,dtype=object)
+
+#print(cards)
+
+###Initial Conditions
+card_wins = []#Find no of wins on each card
+
+###Main Code
+for i_c,card in enumerate(cards):#Evaluates each card individually to determine score
+    winning_numbers = match_numbers(card[1],card[0])
+    no_of_wins = len(winning_numbers)
+    card_wins.append(no_of_wins)
+
+#print(card_wins)
+
+no_of_cards_arr = np.ones(len(card_wins))
+
+for i_d,no_of_wins in enumerate(card_wins):#Iterates through and updates number of cards
+    no_of_cards = no_of_cards_arr[i_d]
+    
+    i_e = 1
+    while i_e <= no_of_wins:
+        no_of_cards_arr[i_d+i_e] += no_of_cards
+        i_e += 1
+
+#print(no_of_cards_arr)
+
+result = sum(no_of_cards_arr)
+
+###Result
+print(result)
+
+timetaken = time.time() - start_time
+print("Completed in ", timetaken, " seconds")
